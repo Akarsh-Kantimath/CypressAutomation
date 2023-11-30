2e94cdf06c522d46b3d656cef808067044fdd8e5
Akarsh Kantimath <akarshkantimath@gmail.com>
2023-11-30 19:28:09 +0530

    Static and Dynamic dropdown handling

--- cypress/Integration/examples/Dropdown_Handling.js
+++ cypress/Integration/examples/Dropdown_Handling.js
@@ -2,7 +2,16 @@ describe('dropdown_handling',function(){
     it('Static Dropdown',function(){
         cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
         cy.get('select').select('Option2').should('have.value','option2') //Static dropdown
+    })
+    it('dynamic Dropdown',function(){
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+        cy.get('.inputs.ui-autocomplete-input').type('ind')
 
-        
+        cy.get('.ui-menu-item').each(($el, index, $list) => {
+            if($el.text() === 'India'){
+                $el.click()
+            }
+        })
+        cy.get('input#autocomplete').should('have.value','India')
     })
 })
